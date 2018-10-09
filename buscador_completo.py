1617fb25ff75b9a82fcc550b907eb23614b753f4
flavio <ffllaa@gmail.com>
2018-10-08 21:39:58 -0300

    Bot updated. Now, during the second try, while looking without exact name, it check the sources. If there is only one this is the exact source name and it is safe to say we found what we were looking. If there is more than one the program compares the names of the sources and the name of the desired source. If there is a match of more than 75% with one source, the program make a third try with this source as exact source name.

--- buscador_completo.py
+++ buscador_completo.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 import time
 
 class Source(object):
-	name
-	soup
+	name = ''
+	soup = ''
 
 def second_try(name):
 	print("second try:", name)
@@ -15,8 +15,9 @@ def second_try(name):
 	url = 'https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?numberOfFields=0&src=s&clickedLink=&edit=&editSaveSearch=&origin=searchbasic&authorTab=&affiliationTab=&advancedTab=&scint=1&menu=search&tablin=&searchterm1={}&field1=SRCTITLE&dateType=Publication_Date_Type&yearFrom=2006&yearTo=Present&loadDate=7&documenttype=All&accessTypes=All&resetFormLink=&st1={}&st2=&sot=b&sdt=b&sl=25&s=SRCTITLE%28{}%29&sid=bcb197c7d0aeae158eb956eccdf7e5a1&searchId=bcb197c7d0aeae158eb956eccdf7e5a1&txGid=0d239eb0691e9b092f906f8218cbddc0&sort=plf-f&originationType=b&rr='.format(name,name,name)
 	driver.get(url)
 	soup = BeautifulSoup(driver.page_source, 'html.parser')
-	try:
-		Areas = soup.find('ul', id = "cluster_SUBJAREA")
+	Source.soup = soup
+	Areas = soup.find('ul', id = "cluster_SUBJAREA")
+	if Areas != None:
 		Areas = Areas.find_all('span', class_="btnText")
 		Areas_text = list(map(lambda area: area.get_text().replace('\n',''), Areas))
 		if ("Social Sciences" in Areas_text):
@@ -24,27 +25,48 @@ def second_try(name):
 			# return 'Unsure'
 		else:
 			return False
-	except:
+	else:
 		return 'Not Found'
 
 def check_duplicity():
 	soup = Source.soup
-	Source_title = soup.find('ud', id_="cluster_EXACTSRCTITLE")
-	Source_title = Source_title.find_all('span', class_='btnText')
+	Source_title = soup.find('div', id ="clusterAttribute_EXACTSRCTITLE")
+	Source_title = Source_title.find_all('label', class_='checkbox-label')
 	if len(Source_title)==1:
 		return 'Ok'
 	else:
-		return 'Unsure'
-	# Source_title = list(map(lambda source: source.get_text(), Source_title))
+		Source_title = list(map(lambda source: source.get_text().replace("\n",""), Source_title))
+		# print(Source_title)
+		exact_name = find_source(Source_title)
+		if exact_name == False:
+			return 'Unsure'
+		else:
+			print("third try: {}".format(exact_name))
+			url = "https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?sort=plf-f&src=s&st1={}&nlo=&nlr=&nls=&sid=65f022fdb8a1532543084f624d780d48&sot=b&sdt=cl&cluster=scoexactsrctitle%2c%22{}%22%2ct&sl=35&s=SRCTITLE%28{}%29+AND+PUBYEAR+%3e+2005&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=564a84a1d55c56449e36e21586ef0bd7".format(exact_name,exact_name,exact_name)
+			driver.get(url)
+			soup = BeautifulSoup(driver.page_source, 'html.parser')
+			Areas = soup.find('ul', id = "cluster_SUBJAREA")
+			Areas = Areas.find_all('span', class_="btnText")
+			Areas_text = list(map(lambda area: area.get_text().replace('\n',''), Areas))
+			if ("Social Sciences" in Areas_text):
+				return 'Probably Ok - checked: {}'.format(exact_name)
+			else:
+				return 'Probably False - checked: {}'.format(exact_name)
 
-
-
+def find_source(source_list):
+	list1 = Source.name.replace('"','').replace('-',' ').replace('&','And').lower().split()
+	for source in source_list:
+		list2 = source.lower().split()
+		intersection = [word for word in list1 if word in list2]
+		if len(intersection)/len(list1)>0.75 and len(intersection)/len(list2)>0.75:
+			return source
+	return False
 	
 
-ini = 0
-results = 100
-first_time = True
-output_filename = "teste_05.10.csv"
+ini = 739
+results = 10
+first_time = False
+output_filename = "full_06.10.csv"
 
 dados = pd.read_csv("JHG.csv", skiprows = 1)
 # print(dados.head())
@@ -60,8 +82,8 @@ if first_time:
 	file.write("Full Journal Title,Journal Impact Factor,Search Result\n")
 	file.close()
 
-nome = "Nature Climate Change"
-url = "https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?sort=plf-f&src=s&st1={}&nlo=&nlr=&nls=&sid=65f022fdb8a1532543084f624d780d48&sot=b&sdt=cl&cluster=scoexactsrctitle%2c%22{}%22%2ct&sl=35&s=SRCTITLE%28{}%29+AND+PUBYEAR+%3e+2005&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=564a84a1d55c56449e36e21586ef0bd7".format(nome,nome,nome)
+name = "Nature Climate Change"
+url = "https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?sort=plf-f&src=s&st1={}&nlo=&nlr=&nls=&sid=65f022fdb8a1532543084f624d780d48&sot=b&sdt=cl&cluster=scoexactsrctitle%2c%22{}%22%2ct&sl=35&s=SRCTITLE%28{}%29+AND+PUBYEAR+%3e+2005&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=564a84a1d55c56449e36e21586ef0bd7".format(name,name,name)
 driver = webdriver.Chrome()
 driver.get(url)
 # time.sleep(25)
@@ -72,11 +94,11 @@ i = ini
 j = 0
 while j < results:
 	next_ = False
-	nome = '"'+titles[i]+'"'
+	name = '"'+titles[i]+'"'
 	exact_name = titles[i].replace('-',' ').title()
 	exact_name = exact_name.replace('&','And')
 	Source.name = exact_name
-	url = "https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?sort=plf-f&src=s&st1={}&nlo=&nlr=&nls=&sid=65f022fdb8a1532543084f624d780d48&sot=b&sdt=cl&cluster=scoexactsrctitle%2c%22{}%22%2ct&sl=35&s=SRCTITLE%28{}%29+AND+PUBYEAR+%3e+2005&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=564a84a1d55c56449e36e21586ef0bd7".format(nome,exact_name,exact_name)
+	url = "https://www-scopus.ez67.periodicos.capes.gov.br/results/results.uri?sort=plf-f&src=s&st1={}&nlo=&nlr=&nls=&sid=65f022fdb8a1532543084f624d780d48&sot=b&sdt=cl&cluster=scoexactsrctitle%2c%22{}%22%2ct&sl=35&s=SRCTITLE%28{}%29+AND+PUBYEAR+%3e+2005&origin=resultslist&zone=leftSideBar&editSaveSearch=&txGid=564a84a1d55c56449e36e21586ef0bd7".format(name,exact_name,exact_name)
 	driver.get(url)
 	soup = BeautifulSoup(driver.page_source, 'html.parser')
 	Source.soup = soup
@@ -89,7 +111,7 @@ while j < results:
 			trues.append(i)
 			j += 1
 	except:
-		result = second_try(nome)
+		result = second_try(name)
 		if result != False:
 			context.append(result)
 			trues.append(i)
